f57b1512be06788d12bd9a3b5f9a6513e93cb0b1
MahfujAhsan <ahsanmahfuj@gmail.com>
2023-04-04 20:15:58 +0600

    address corrected

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://technotes-server-api.render.com',
+    baseUrl: 'https://technotes-server-api.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
